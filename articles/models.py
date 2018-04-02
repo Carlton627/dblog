78e69480622a97b9c1d88628611271f63ac3990d
Carlton <carlton_rodrigues21@outlook.com>
2018-04-02 23:28:59 +0530

    Initial commit

---
+++ articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=50, default="Anonymous")
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    thumb = models.ImageField(default='default.png', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    def snippet(self):
+        return self.body[:100]+'...'
+
+
+
